63d9c799c3dd5b2207c10f96bcbfaa25fa9f81f6
chris-smekens-vinci <chris.smekens@student.vinci.be>
2026-02-03 11:15:29 +0100

    feat: add validation for comment content to ensure proper punctuation

--- backend/src/comments/dto/create-comment.dto.ts
+++ backend/src/comments/dto/create-comment.dto.ts
@@ -1,6 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsString, Matches } from 'class-validator';
 
 export class CreateCommentDto {
   @IsString()
+  @Matches(/.*[.!?]$/, {
+    message: 'Le commentaire doit se terminer par un ".", "?" ou "!"',
+  })
   content: string;
 }
