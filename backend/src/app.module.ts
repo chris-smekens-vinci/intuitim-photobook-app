3eebb55104375f94fc0215bd1897a27634196a19
chris-smekens-vinci <chris.smekens@student.vinci.be>
2026-02-06 10:57:06 +0100

    test: add comments to clarify the purpose of AppModule and its components

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,12 @@ import { CategoriesModule } from './categories/categories.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/* His role is to orchestrate the different modules of the application and to set up the database connection. 
+It imports the PhotosModule and CategoriesModule, which contain the logic for handling photos and categories,
+ respectively. It also sets up the Mongoose connection to MongoDB using the MONGODB_URI environment variable or a default local URI. 
+ Finally, it defines the AppController and AppService as part of the application. */
+
+// He assures that all parts of the application are properly wired together and can communicate with each other as needed.
 @Module({
   imports: [
     MongooseModule.forRoot(
