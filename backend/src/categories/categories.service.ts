48e8101a8cb170c0f0c5d3be1910d0536810c440
chris-smekens-vinci <chris.smekens@student.vinci.be>
2026-02-06 11:52:49 +0100

    test: add comment to clarify the purpose of @Injectable() in CategoriesService

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Category } from './schemas/category.schema';
 
+// Used to declare a class as a provider that can be injected via dependency injection.
 @Injectable()
 export class CategoriesService {
   constructor(
