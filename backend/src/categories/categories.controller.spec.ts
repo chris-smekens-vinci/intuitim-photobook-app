f8bf7fd9ee4809dd324e1f70ae441e4464c5823c
chris-smekens-vinci <chris.smekens@student.vinci.be>
2026-02-06 10:00:08 +0100

    test: refactor CategoriesController tests to use TestingModule

--- backend/src/categories/categories.controller.spec.ts
+++ backend/src/categories/categories.controller.spec.ts
@@ -1,5 +1,6 @@
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { Test, TestingModule } from '@nestjs/testing';
 
 // Dans CategoriesController
 //constructor(private readonly categoriesService: CategoriesService) {}
@@ -9,17 +10,34 @@ describe('CategoriesController', () => {
   let categoriesController: CategoriesController;
   let categoriesService: CategoriesService;
 
-  beforeEach(() => {
-    categoriesService = new CategoriesService(null);
-    categoriesController = new CategoriesController(categoriesService);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      // Configuration du module de test
+      controllers: [CategoriesController],
+      providers: [
+        {
+          provide: CategoriesService,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    categoriesController =
+      module.get<CategoriesController>(CategoriesController);
+    categoriesService = module.get<CategoriesService>(CategoriesService);
   });
 
   describe('findAll', () => {
     it('should return an array of categories', async () => {
-      const result = ['test'];
-      jest.spyOn(categoriesService, 'findAll').mockImplementation(() => result);
+      const mockCategories = [{ name: 'Category1' }, { name: 'Category2' }];
+      jest
+        .spyOn(categoriesService, 'findAll')
+        .mockResolvedValue(mockCategories as unknown as any);
 
-      expect(await catsController.findAll()).toBe(result);
+      const categories = await categoriesController.findAll();
+      expect(categories).toEqual(mockCategories);
     });
   });
 });
